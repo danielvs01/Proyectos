1b0d91becc9362068ad3ab72120aaa9b060b2173
Daniel Vidal Solano <danielvsolano1@gmail.com>
2023-09-20 18:20:11 -0600

    guardado 1

--- Peliculas/sistema/pelicula/urls.py
+++ Peliculas/sistema/pelicula/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('inicio/', views.inicio, name='inicio'),
     path('peliculas/', views.peliculas, name='peliculas'),
+    path('nosotros/', views.nosotros, name='nosotros'),
 ]
